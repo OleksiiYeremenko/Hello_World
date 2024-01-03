e8d7f10d5b51ee725707a67ea5d8003954a58585
OleksiiYeremenko <aleksejjerjomenko09021993@gmail.com>
2024-01-03 14:08:06 +0100

    Changed HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,5 +3,6 @@ package main.java;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello GOIT JAVA World");
+        System.out.println("This is my second commit");
     }
 }
